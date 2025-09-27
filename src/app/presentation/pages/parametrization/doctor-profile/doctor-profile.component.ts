c525817260da2c6002c0d22fd513565927469641
joelhuba <joelhuba1999@gmail.com>
2025-09-26 22:35:04 -0500

    creacion de componente de doctor profile

--- src/app/presentation/pages/parametrization/doctor-profile/doctor-profile.component.ts
+++ src/app/presentation/pages/parametrization/doctor-profile/doctor-profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DoctorProfileDTO } from 'src/app/core/DTOs/app/doctor-profile-dto';
+import { DoctorProfileUseCase } from '../../../../infrastructure/use-cases/app/doctor-profile-use-case';
+import { NotificationsService } from 'src/app/infrastructure/services/common/notifications/notifications.service';
 
 @Component({
   standalone: true,
@@ -7,8 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './doctor-profile.component.css'
 })
 export class DoctorProfileComponent implements OnInit {
+  doctorProfile: DoctorProfileDTO[] = [];
+  isLoading: boolean = false;
+  modalRef?: any;
+  constructor(
+    private router: Router,
+    private _doctorProfileUseCase: DoctorProfileUseCase,
+    private _notificationService: NotificationsService
+
+  ) { }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
+  loadDoctorProfile() {
+    this.isLoading = true;
+    
+  }
 }
